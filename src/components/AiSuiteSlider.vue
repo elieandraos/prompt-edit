60aa320564adc8010aa414f53dd376a576a8ab59
Elie Andraos <elieandraos31@gmail.com>
2026-03-08 11:37:41 +0200

    :truck: Use webp images

--- src/components/AiSuiteSlider.vue
+++ src/components/AiSuiteSlider.vue
@@ -65,12 +65,12 @@
 <script setup>
 import { computed, onMounted, ref } from "vue"
 
-import slide1 from "../assets/slide-1.png"
-import slide2 from "../assets/slide-2.png"
-import slide3 from "../assets/slide-3.png"
-import slide4 from "../assets/slide-4.png"
-import slide5 from "../assets/slide-5.png"
-import slide6 from "../assets/slide-6.png"
+import slide1 from "../assets/slide-1.webp"
+import slide2 from "../assets/slide-2.webp"
+import slide3 from "../assets/slide-3.webp"
+import slide4 from "../assets/slide-4.webp"
+import slide5 from "../assets/slide-5.webp"
+import slide6 from "../assets/slide-6.webp"
 
 const slides = [
     {
